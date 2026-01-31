be719ca394b88b667d4217c8ad958e37f5cbf723
Yusuke <hoso.with.ai@gmail.com>
2026-02-01 08:10:03 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,12 +1,18 @@
+#utils.py
 import pandas as pd
 import numpy as np
 import rpy2.robjects as robjects
 from rpy2.robjects import pandas2ri, numpy2ri
 from rpy2.robjects.packages import importr
+from rpy2.robjects import conversion # 追加
 
-pandas2ri.activate()
-numpy2ri.activate()
+# --- ここから環境設定 ---
+# activate() の代わりに、一括で変換ルールを登録します
+my_converter = robjects.default_converter + pandas2ri.converter + numpy2ri.converter
+conversion.set_conversion(my_converter)
+
 sommer = importr('sommer')
+# --- 環境設定ここまで ---
 
 def load_genomic_data(pheno_path, geno_path):
     df_pheno = pd.read_csv(pheno_path)
@@ -21,7 +27,8 @@ def load_genomic_data(pheno_path, geno_path):
     
     df_geno = pd.read_csv(geno_path)
     mapping = {'A': 0, 'H': 1, 'B': 2, '-': np.nan, 'N': np.nan}
-    geno_data = df_geno.iloc[:, 5:].replace(mapping).infer_objects(copy=False).T
+    # T.copy() を入れて明示的にコピーを作成し、型エラーを防ぎます
+    geno_data = df_geno.iloc[:, 5:].replace(mapping).infer_objects(copy=False).T.copy()
     geno_data.index.name = 'Corrected Strain'
     geno_data = geno_data.fillna(geno_data.mean())
     
@@ -29,16 +36,19 @@ def load_genomic_data(pheno_path, geno_path):
     y_multi = combined[target_cols].astype(np.float64).copy()
     y_multi.columns = [traits_map[c] for c in y_multi.columns]
     X = combined.iloc[:, len(target_cols) + 1 :].values.astype(np.float32)
-    return X, y_multi.fillna(y_multi.mean())
+    return X, y_multi.fillna(y_multi.mean()), combined['Corrected Strain'].tolist()
 
 def calculate_gblup_residuals(X, y_df, train_idx, test_idx, strain_ids):
+    # numpyのデータをRに渡すために変換器を有効にした状態で処理
     X_std = (X - X.mean(axis=0)) / (X.std(axis=0) + 1e-6)
+    # G行列計算
     G = (np.dot(X_std, X_std.T) / X.shape[1]) + np.eye(len(X)) * 1e-5
     
     df_r = y_df.copy()
     df_r['id'] = strain_ids
     df_r.loc[test_idx, 'Yield'] = np.nan 
 
+    # Rのグローバル環境へデータを転送
     robjects.globalenv['df_r'] = df_r
     robjects.globalenv['G_mat'] = G
     robjects.globalenv['strain_ids_r'] = robjects.StrVector(strain_ids)
@@ -47,6 +57,7 @@ def calculate_gblup_residuals(X, y_df, train_idx, test_idx, strain_ids):
     library(sommer)
     rownames(G_mat) <- strain_ids_r; colnames(G_mat) <- strain_ids_r
     
+    # sommerによる多変量GBLUP
     ans_mt <- mmer(fixed = cbind(Yield, DaysToMat, Protein, SeedWeight) ~ 1,
                    random = ~ vsr(id, Gu=G_mat, Gtc=unsm(4)), 
                    rcov = ~ vsr(units, Gtc=diag(4)), data = df_r, verbose = FALSE)
@@ -55,9 +66,20 @@ def calculate_gblup_residuals(X, y_df, train_idx, test_idx, strain_ids):
     u_mt_yield <- u_mt_mat[strain_ids_r, 1] 
     res_mat <- as.matrix(ans_mt$residuals)
     full_res <- rep(0, length(strain_ids_r))
-    full_res[as.numeric(rownames(res_mat))] <- as.numeric(res_mat[, 1])
+    
+    # 欠損値（test_idx）がある場合の残差補完
+    existing_idx <- as.numeric(rownames(res_mat))
+    full_res[existing_idx] <- as.numeric(res_mat[, 1])
     
     list(u_mt = u_mt_yield, res = full_res)
     """
     res = robjects.r(r_script)
-    return np.array(res.rx2('u_mt')), np.array(res.rx2('res'))
+
+    # res.names に () をつけて呼び出すように修正します
+    names = list(res.names()) 
+    u_mt_idx = names.index('u_mt')
+    res_idx = names.index('res')
+
+    return np.array(res[u_mt_idx]), np.array(res[res_idx])
+
+print("関数の定義が完了しました。")
